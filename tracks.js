95c677ffcedcb80955b0a35630b9cc2b91fcda39
Alex McVittie <alexander.mcvittie@gmail.com>
2025-06-12 13:21:12 -0700

    Reorg of images, conversion to webp

--- tracks.js
+++ tracks.js
@@ -2,7 +2,7 @@ const tracks = [
   {
     title: "Gloryhammer - Wasteland Warrior Hoots Patrol",
     file: "audio/HOOTS.mp3",
-    artwork: "img/GH_WWHP.jpg",
+    artwork: "img/portfolio/GH_WWHP.webp",
     credits: ["Saxophone"],
     genres: ["Rock", "Power Metal"],
     //disclaimer: "Not the official mix.\nCourtesy of XYZ Academy."
@@ -10,56 +10,56 @@ const tracks = [
   {
     title: "Suite Clarity - Led To Disgrace",
     file: "audio/SuiteClarity.m4a",
-    artwork: "img/convergence.jpg",
+    artwork: "img/portfolio/convergence.webp",
     credits: ["Keyboards"],
     genres: ["Progressive Rock"]
   },
   {
     title: "Last II - Black Hole",
     file: "audio/BlackHole.mp4",
-    artwork: "img/last2.jpg",
+    artwork: "img/portfolio/last2.webp",
     credits: ["Keyboards"],
     genres: ["Rock", "Metal"]
   },
   {
     title: "FXRLY - I'll Be There",
     file: "audio/bthere.mp3",
-    artwork: "img/FXLRY.jpg",
+    artwork: "img/portfolio/FXLRY.webp",
     credits: ["Keyboards"],
     genres: ["Pop Punk"]
   },
   {
     title: "Tyler Montague March - Pink Pony Club",
     file: "audio/pinkpony.mp3",
-    artwork: "img/pinkpony.png",
+    artwork: "img/portfolio/pinkpony.webp",
     credits: ["Mastering", "Mixing", "Keyboards", "Engineering", "Produced"],
     genres: ["Contemporary Country"]
   },
   {
     title: "McVittie - Adrenaline",
     file: "audio/Adrenaline.mp3",
-    artwork: "img/adrenaline.jpg",
+    artwork: "img/portfolio/adrenaline.webp",
     credits: ["Keyboards", "Produced", "Mixing", "Mastering", "Writing", "Engineering"],
     genres: ["Electronic", "Synthwave"]
   },
   {
     title: "Spectra - Meridian",
     file: "audio/Meridian.mp3",
-    artwork: "img/pwodo.jpg",
+    artwork: "img/portfolio/pwodo.webp",
     credits: ["Keyboards", "Writing", "Produced", "Mastering", "Mixing", "Engineering"],
     genres: ["Progressive Rock"]
   },
   {
     title: "McVittie - Lone Killer",
     file: "audio/LoneKiller.mp3",
-    artwork: "img/lonekiller.jpg",
+    artwork: "img/portfolio/lonekiller.webp",
     credits: ["Keyboards", "Produced", "Mixing", "Mastering", "Writing", "Engineering"],
     genres: ["Electronic", "Synthwave"]
   },
   {
     title: "Crimson Ash - In Twilight",
     file: "audio/InTwilight.mp3",
-    artwork: "img/meltingsky.jpg",
+    artwork: "img/portfolio/meltingsky.webp",
     credits: ["Mixing", "Mastering"],
     genres: ["Progressive Rock"]
   },
@@ -67,35 +67,35 @@ const tracks = [
   {
     title: "Reggo The God - Birds Eye View",
     file: "audio/birdseyeview.mp3",
-    artwork: "img/birdseye.png",
+    artwork: "img/portfolio/birdseye.webp",
     credits: ["Mastering", "Mixing"],
     genres: ["Hip Hop"]
   },
   {
     title: "Spectra - Lost At Sea",
     file: "audio/LostAtSea.mp3",
-    artwork: "img/pwodo.jpg",
+    artwork: "img/portfolio/pwodo.webp",
     credits: ["Keyboards", "Saxophone", "Writing", "Produced", "Mastering", "Mixing", "Engineering"],
     genres: ["Progressive Rock"]
   },
   {
     title: "McVittie - The Final Kill",
     file: "audio/FinalKill_Sax.mp3",
-    artwork: "img/lonekiller.jpg",
+    artwork: "img/portfolio/lonekiller.webp",
     credits: ["Keyboards", "Saxophone", "Produced", "Mixing", "Mastering", "Writing", "Engineering"],
     genres: ["Electronic", "Synthwave"]
   },  
   {
     title: "Mortrem - Nonfiction",
     file: "audio/nonfiction.mp3",
-    artwork: "img/Mortrem.jpg",
+    artwork: "img/portfolio/Mortrem.webp",
     credits: ["Keyboards"],
     genres: ["Progressive Rock"]
   },  
   {
     title: "McVittie - Ionosphere",
     file: "audio/ionosphere.mp3",
-    artwork: "img/voltage.jpg",
+    artwork: "img/portfolio/voltage.webp",
     credits: ["Keyboards", "Produced", "Mixing", "Mastering", "Writing", "Engineering"],
     genres: ["Electronic", "Synthwave"]
   },
